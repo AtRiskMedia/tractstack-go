17f91f254935384cd09c014cd19b3d9921809612
Adon <a@atriskmedia.com>
2026-01-13 01:35:31 -0500

    allow base64 images

--- internal/presentation/templates/hast_renderer.go
+++ internal/presentation/templates/hast_renderer.go
@@ -32,6 +32,7 @@ func (hr *HastRenderer) Render(ast *rendering.HTMLAST) string {
 	}
 
 	p := bluemonday.UGCPolicy()
+	p.AllowDataURIImages()
 	p.AllowRelativeURLs(true)
 	p.AllowAttrs("class", "style", "id", "data-callback", "data-bunny-payload", "data-external").Globally()
 
